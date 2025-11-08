deee55081b37e298e85928528913ba01910aa505
Shaurya <shaurya.tiwari2507@gmail.com>
2025-11-08 23:03:20 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shaurya",
+    author_email="shaurya.tiwari2507@gmail.com",
+    packages=find_packages()
+)
